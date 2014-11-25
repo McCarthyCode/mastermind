f37617a3a423ec58d964d7559e3c9d39ec81d455
lzapar2 <lmzapart@gmail.com>
2014-11-25 13:11:56 -0600

    Update Game.java
    Game object update. Take 1 through the website.

--- src/Game.java
+++ src/Game.java
@@ -1,24 +1,225 @@
+package mastermind;
+
+import java.util.Scanner;
+
 public class Game {
-	// Attributes
-	private int computerGuess[];
-	private int computerMatches[];
-	private Player computerPlayer;
+
+	private void displayResults() {
+		if (winner == 1) {
+			System.out.println("YOU WIN!!!");
+		} else if (winner == 2) {
+			System.out.println("YOU LOSE!!!");
+		} else {
+			System.out.println("DRAW!!!");
+			System.out.println("Both players guessed the combination in equal number of turns.");
+		}
+	}
+
+	private int[] getGuess(Player p) {
+		return p.guess();
+	}
 	
-	private int humanGuess[];
-	private int humanMatches[];
-	private Player humanPlayer;
+	public Game() {
+		
+	}
+
+	private void checkResults() {
+
+		// n == -2 means number does not exist within opponent's combination
+		// n == -1 means digit exist within opponent's combination, but in
+		// different slot
+		// n >= 0 means that user guessed the correct digit in the correct slot
+		// in that case, we set n to the actual digit
+		for (int i = 0; i < 4; i++) {
+			humanMatches[i] = -2;
+			computerMatches[i] = -2;
+		}
+
+		// contains number of digits a player guessed correctly
+		int numHumanMatches = 0;
+		int numComputerMatches = 0;
+
+		boolean compDone = false;
+		boolean humanDone = false;
+
+		// evaluate matches and number of matches for each player
+		// the done flags are set for when a matching number is found
+		// to limit number of unnecessary checks
+		// and combine checks for both players in one loop
+		for (int i = 0; i < 4; i++) {
+			compDone = false;
+			humanDone = false;
+			for (int j = 0; j < 4; i++) {
+				if (!humanDone && (humanGuess[i] == computerCode[j])) {
+					numHumanMatches++;
+					if (i == j) // matching digits and matching slots
+						humanMatches[i] = computerCode[j];
+					else
+						humanMatches[i] = -1;
+				}
+
+				if (!compDone && (computerGuess[i] == humanGuess[j])) {
+					numComputerMatches++;
+					if (i == j) // matching digits and matching slots
+						computerMatches[i] = humanCode[j];
+					else
+						computerMatches[i] = -1;
+				}
+			}
+		}
+
+		if (numHumanMatches == 4)
+			winner = 1;
+		if (numComputerMatches == 4)
+			winner = 2;
+
+		if (numHumanMatches == 4 && numComputerMatches == 4)
+			winner = 3;
+	}
+
+	private void notifyPlayer(Player p, int[] matches) {
+
+	}
+
+	private void playAgain() {
+		System.out.println("Would you like to play again?");
+		System.out.println("1 - Yes");
+		System.out.println("2 - No");
+		replay = scan.nextInt();
+	}
+
+	private void reset() {
+		winner = 0;
+		replay = 1;
+
+		computerGuess = new int[4];
+		computerMatches = new int[4];
+		humanGuess = new int[4];
+		humanMatches = new int[4];
+	}
+
+	private void getSettings() {
+
+		System.out.println("Choose level difficulty:");
+		System.out.println("1 - Easy");
+		System.out.println("2 - Normal");
+		System.out.println("3 - Hard");
+		levelChoice = scan.nextInt();
+
+		System.out.println("Select opponent intellect:");
+		System.out.println("1 - Dumb");
+		System.out.println("2 - Normal");
+		System.out.println("3 - Smart");
+		opponentChoice = scan.nextInt();
+
+	}
+
+	Player humanPlayer;
+	Player computerPlayer;
 	
-	private LevelFactory factory;
-	private int replay;
-	private int winner;
-	
-	// Operations
-	private boolean checkResult( int a[], int b[]) {return false;}
-	private void displayResults() {}
-	private int[] getGuess( Player player ) {return null;}
-	private void getSettings() {}
-	private void notifyPlayer( Player player, int results[] ) {}
-	private void reset() {}
-	private int[] sendGuess( Player player, int guess[] ) {return null;}
-	public static void main( String args[] ) {}
+	int levelChoice;
+	int[] humanCode;
+	int[] computerCode;
+
+	int[] computerGuess;
+	int[] computerMatches;
+	int[] humanGuess;
+	int[] humanMatches;
+
+	int winner = 0;
+	int replay = 1;
+	int opponentChoice;
+
+	Scanner scan;
+	LevelFactory factory;
+
+	public void runGame() {
+		scan = new Scanner(System.in);
+		while (replay > 1) {
+			
+			getSettings();
+			
+			// 1 == easy, 3 == hard, 2 and any other input == normal
+			switch (levelChoice) {
+			case 1:
+				factory = new EasyLevelFactory();
+				break;
+			case 3:
+				factory = new HardLevelFactory();
+				break;
+			default:
+				factory = new MediumLevelFactory();
+				break;
+			}
+			
+			while (replay == 1) {
+
+				humanCode = getCode(HumanPlayer);
+				
+				while (winner == 0) {
+					humanGuess = getGuess(humanPlayer);
+
+					computerGuess = getGuess(computerPlayer);
+
+					notifyPlayer(computerPlayer, computerMatches);
+					notifyPlayer(humanPlayer, humanMatches);
+
+					checkResults();
+
+					if (winner > 0) {
+						displayResults();
+						reset();
+						playAgain();
+					}
+				}
+
+			}
+		}
+	}
+/*
+	public abstract class Player {
+		public int[] guess() {
+			return null;
+		}
+	}
+
+	public class HumanPlayer extends Player {
+		public int[] guess() {
+			return null;
+		}
+
+		public int[] getInput() {
+			return null;
+		}
+	}
+
+	public class ComputerPlayer extends Player {
+		public int[] guess() {
+			return null;
+		}
+
+	}
+
+	public interface LevelFactory {
+		int[] createLevel();
+	}
+
+	public class EasyLevelFactory implements LevelFactory {
+		public int[] createLevel() {
+			return null;
+		}
+	}
+
+	public class MediumLevelFactory implements LevelFactory {
+		public int[] createLevel() {
+			return null;
+		}
+	}
+
+	public class HardLevelFactory implements LevelFactory {
+		public int[] createLevel() {
+			return null;
+		}
+	}
+	*/
 }

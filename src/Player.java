0e2028dd347dc5a51353d07f570277b4986df9c1
Matt McCarthy <mattmc318@gmail.com>
2014-11-25 13:40:08 -0600

    More changes to Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,95 +1,76 @@
 import java.util.Scanner;
 
-public abstract class Player{
-
-	private int[] answer;
+public abstract class Player {
+	protected int[] answer;
 	private int[] correctGuesses;
 
-	public Player ()
-	{
+	public Player () {}
+	public Player(int[] answer)	{this.answer = answer;}
 
-	}
-	public Player(int[] answer)
-	{
-			this.answer = answer;
-	}
+	public abstract int[] guess();
 
-	/*
-	public int[] checkMatches(int[] guess)
-	{
-
-	}
-	**/
-
-	public abstract int[] guess()
-	{
-	}
-
-	public abstract void setCode(int[] code)
-	{
-
-	}
-
+	public abstract void setCode(int[] code);
 }
 
 class HumanPlayer extends Player
 {
+	public HumanPlayer() // default constructor
+	{
+		super();
+	}
+	public HumanPlayer(int[] answer)
+	{
+		super(answer);
+	}
+	public int[] guess()
+	{
+		Scanner input = new Scanner(System.in);
+		int a,b,c,d;
+		a = input.nextInt();
+		b = input.nextInt();
+		c = input.nextInt();
+		d = input.nextInt();
+		int[] guess = {a,b,c,d};
+		return guess;
+	}
 
-        public HumanPlayer() // default constructor
-        {
-                super();
-        }
-        public HumanPlayer(int[] answer)
-        {
-                super(answer);
-        }
-        public int[] guess()
-        {
-                Scanner input = new Scanner(System.in);
-                int a,b,c,d;
-                a = input.nextInt();
-                b = input.nextInt();
-                c = input.nextInt();
-                d = input.nextInt();
-                int[] guess = {a,b,c,d};
-                return guess;
-        }
+	public void setCode(int[] code)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter 4 digits for initial code: ");
 
-        public void setCode()
-        {
-                Scanner input = new Scanner(System.in);
-                System.out.println("Enter 4 digits for initial code: ");
+		this.answer[0] = input.nextInt();
+		this.answer[1] = input.nextInt();
+		this.answer[2] = input.nextInt();
+		this.answer[3] = input.nextInt();
 
-                this.answer[0] = input.nextInt();
-                this.answer[1] = input.nextInt();
-                this.answer[2] = input.nextInt();
-                this.answer[3] = input.nextInt();
-
-        }
+	}
 }
 
 class ComputerPlayer extends Player
 {
 
-        private Intellect strategy;
+	private Intellect strategy;
 
-        public ComputerPlayer()
-        {
-                super();
-        }
-        public ComputerPlayer(int[] answer, Intellect strategy)
-        {
-                super(answer);
-                this.strategy = strategy;
-        }
+	public ComputerPlayer()
+	{
+		super();
+	}
+	public ComputerPlayer(int[] answer, Intellect strategy)
+	{
+		super(answer);
+		this.strategy = strategy;
+	}
 
-        public void setIntellect(Intellect strategy)
-        {
-                this.strategy = strategy;
-        }
+	public void setIntellect(Intellect strategy)
+	{
+		this.strategy = strategy;
+	}
 
-        public int[] guess()
-        {
-
-        }
+	public int[] guess()
+	{
+		return null;
+	}
+	
+	public void setCode(int[] code) {}
 }
